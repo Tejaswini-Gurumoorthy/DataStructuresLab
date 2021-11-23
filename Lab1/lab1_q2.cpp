b3b8ecba05afec045797cbc3f5d19390874a23c6
Tejaswini-Gurumoorthy <75904755+Tejaswini-Gurumoorthy@users.noreply.github.com>
2021-11-23 09:15:19 +0530

    Update lab1_q2.cpp

--- Lab1/lab1_q2.cpp
+++ Lab1/lab1_q2.cpp
@@ -3,61 +3,67 @@ using namespace std;
 
 class Student
 {
-  char name[50];
-  int roll_no;
-  char grade;
+    char name[50];
+    int roll_no;
+    char grade;
 
 public:
-  void read(Student s[], int size)
-  {
-    for (int i = 0; i < size; i++)
+    void read(Student s[], int size)
     {
-      cout << "Enter Student name : ";
-      cin >> s[i].name;
-      cout << "Enter student roll number : ";
-      cin >> s[i].roll_no;
-      cout << "Enter student grade : ";
-      cin >> s[i].grade;
-    }
-  }
-
-  void display(Student s[], int size)
-  {
-    s->sort(s, size);
-    for (int i = 0; i < size; i++)
-    {
-      cout << "Student name : " << s[i].name << endl;
-      cout << "Student roll number : " << s[i].roll_no << endl;
-      cout << "Enter student grade : " << s[i].grade << endl;
-    }
-  }
-  void sort(Student s[], int size)
-  {
-    int min, temp, pos;
-    for (int i = 0; i < size; i++)
-    {
-      min = s[i].roll_no;
-      for (int j = i + 1; j < size; j++)
-      {
-        if (s[j].roll_no < min)
+        for (int i = 0; i < size; i++)
         {
-          min = s[j].roll_no;
-          pos = j;
+            cout << "Enter Student name : ";
+            cin >> s[i].name;
+            cout << "Enter student roll number : ";
+            cin >> s[i].roll_no;
+            cout << "Enter student grade : ";
+            cin >> s[i].grade;
         }
-      }
-      temp = s[pos].roll_no;
-      s[pos].roll_no = s[i].roll_no;
-      s[i].roll_no = temp;
     }
-  }
+
+    void display(Student s[], int size)
+    {
+        s->sort(s, size);
+        for (int i = 0; i < size; i++)
+        {
+            cout << "DETAILS OF STUDENT " << i + 1 << ": "<<endl;
+            cout << endl;
+            cout << "Student name : " << s[i].name << endl;
+            cout << "Student roll number : " << s[i].roll_no << endl;
+            cout << "Student grade : " << s[i].grade << endl;
+            cout << endl;
+            cout << endl;
+        }
+    }
+    void sort(Student s[], int size)
+    {
+        int min, pos;
+        Student temp;
+        for (int i = 0; i < size; i++)
+        {
+            min = s[i].roll_no;
+            for (int j = i + 1; j < size; j++)
+            {
+                if (s[j].roll_no < min)
+                {
+                    min = s[j].roll_no;
+                    pos = j;
+                    cout << j << endl;
+                }
+            }
+            temp = s[pos];
+            s[pos] = s[i];
+            s[i] = temp;
+        }
+    }
 };
 
 int main()
 {
-  int size;
-  cout << "Enter number of students : ";
-  cin >> size;
-  Student s[size];
-  s->read(s, size);
-  s->display(s, size);
+    int size;
+    cout << "Enter number of students : ";
+    cin >> size;
+    Student s[size];
+    s->read(s, size);
+    s->display(s, size);
 }
